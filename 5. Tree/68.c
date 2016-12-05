d6fdcc6598f47aa1cf659e61aaf84be2d4633236
sanju784 <sanjunitr51@gmail.com>
2016-12-06 00:21:31 +0900

    Trim a tree so that all elements of it are in the range n1 and n2

---
+++ 5. Tree/68.c
@@ -0,0 +1,80 @@
+/*********************************************
+*
+*  Trim a tree so that all elements of it are in the range n1 and n2
+*
+*/////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treeNode{
+  int data;
+  struct treeNode *left;
+  struct treeNode *right;
+} node;
+
+node *newNode(int n) {
+  node *p = (node*)malloc(sizeof(node));
+  p->data = n*3;
+  p->left = NULL;
+  p->right = NULL;
+  return p;
+}
+
+/**
+     4
+    / \
+  2     6
+ / \   / \
+1   3 5   7
+
+*/
+node *createBST() {
+  node *p = newNode(4);
+  p->left = newNode(2);
+  p->right = newNode(6);
+  p->left->left = newNode(1);
+  p->left->right = newNode(3);
+  p->right->left = newNode(5);
+  p->right->right = newNode(7);
+  return p;
+}
+
+node* trimTree(node *root, int n1, int n2) {
+  if(root == NULL)
+    return NULL;
+
+  root->left = trimTree(root->left, n1, n2);
+  root->right = trimTree(root->right, n1, n2);
+  if(root->data < n1) {
+    node *rChild = root->right;
+    free(root);
+    return rChild;
+  }
+  if(root->data > n2) {
+    node *lChild = root->left;
+    free(root);
+    return lChild;
+  }
+  return root;
+}
+
+void inorder(node *root) {
+  if(root) {
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+  }
+}
+
+void main() {
+  node *root = createBST();
+  int n1, n2;
+  printf("\nEnter range : ");
+  scanf("%d %d", &n1, &n2);
+  printf("\nInorder before trim  ");
+  inorder(root);
+  root = trimTree(root, n1, n2);
+  printf("\nInorder after trim  ");
+  inorder(root);
+}

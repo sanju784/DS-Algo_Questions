f752437d694c0b6e90e0f20384944d2773012897
sanju784 <sanjunitr51@gmail.com>
2017-01-04 19:40:22 +0900

    Search element in binary tree - Iteration

---
+++ 5. Tree/4.cpp
@@ -0,0 +1,72 @@
+/*********************************************
+*
+*  Search element in binary tree - Iteration
+*
+*/////////////////////////////////////////////
+
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+#include <stdbool.h>
+
+using namespace std;
+
+typedef struct tree {
+  int data;
+  struct tree *left, *right;
+} node;
+
+queue <node*> q;
+
+node *create() {
+  node* p;
+  int n;
+  cout<<"\nEnter data (Enter -1 if no child node) : ";
+  cin>>n;
+  if(n == -1) {
+    return NULL;
+  }
+  p = (node*)malloc(sizeof(node));
+  p->data = n;
+  cout<<"\nEnter left child of "<<n;
+  p->left = create();
+  cout<<"\nEnter right child of "<<n;
+  p->right = create();
+  return p;
+}
+
+//searching in tree in iterative way
+//using level order traversal
+bool findInTree(node *p, int n) {
+  if(!p) return false;
+  node *temp;
+  q.push(p);
+  while(!q.empty()) {
+    temp = q.front();
+    q.pop();
+    if (n == temp->data) {
+      return true;
+    }
+    if(temp->left) {
+      q.push(temp->left);
+    }
+    if(temp->right) {
+      q.push(temp->right);
+    }
+  }
+  return false;
+}
+
+int main() {
+  node* root;
+  root = create();
+  int n;
+  cout<<"\nEnter the number you want to search. ";
+  cin>>n;
+  bool res = findInTree(root, n);
+  if(res)
+    cout<<n<<" exists in tree.\n";
+  else
+    cout<<n<<" does not exist in tree.\n";
+  return 0;
+}

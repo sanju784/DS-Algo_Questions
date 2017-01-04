7daa1ebccab6f759e29f9bf99261653d291d35bc
sanju784 <sanjunitr51@gmail.com>
2017-01-04 22:57:34 +0900

    Maximum sum in Sliding Window using array

--- 4. Queue/4.cpp
+++ 4. Queue/4.cpp
@@ -10,24 +10,18 @@
 using namespace std;
 
 int main() {
-  int arr[] = {1,3,-1,-3,5,3,6,7};
+  int arr[] = {3,1,-1,-3,5,3,6,7};
   int w = 3;
   int n = sizeof(arr)/sizeof(int);
   int *res;
   res = (int*)malloc((n-w+1)*sizeof(int));
-  int max = arr[0];
-  for(int j = 1;j < w; j++) {
-    if(arr[j] > max) {
-        max = arr[j];
-    }
-  }
-  int j = 0;
-  res[j++] = max;
-  for (int i = w;i < n; i++){
-    if(arr[i] > max) {
-        max = arr[i];
-    }
-    res[j++] = max;
+  for (int i = 0;i < (n - w + 1); i++) {
+	  int max = arr[i];
+	  for (int j = i + 1; j < (i + w); j++) {
+		  if(arr[j] > max)
+			  max = arr[j];
+	  }
+	  res[i] = max;
   }
   cout<<"\nOriginal array is\n";
   for(int i = 0; i < n; i++)

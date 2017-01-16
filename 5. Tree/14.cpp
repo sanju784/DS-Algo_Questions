1e1890313263a4a015f3642934ae04923bdc2108
sanju784 <sanjunitr51@gmail.com>
2017-01-16 20:11:26 +0900

    Find number of leaves in binary tree

---
+++ 5. Tree/14.cpp
@@ -0,0 +1,62 @@
+/*********************************************
+*
+*  Find number of leaves in binary tree
+*
+*/////////////////////////////////////////////
+
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct tree {
+  int data;
+  struct tree *left, *right;
+} node;
+
+queue <node*> q;
+
+node *create() {
+  node* p;
+  int n;
+  cout<<"\nEnter data (Enter -1 if no child node) : ";
+  cin>>n;
+  if(n == -1) {
+    return NULL;
+  }
+  p = (node*)malloc(sizeof(node));
+  p->data = n;
+  cout<<"\nEnter left child of "<<n;
+  p->left = create();
+  cout<<"\nEnter right child of "<<n;
+  p->right = create();
+  return p;
+}
+
+int totalLeaves(node *p) {
+  if(!p)return 0;
+  q.push(p);
+  node *temp;
+  int count = 0;
+  while(!q.empty()) {
+    temp = q.front();
+    q.pop();
+    if (!temp->left && !temp->right) {
+        count++;
+    } else {
+      if(temp->left)
+        q.push(temp->left);
+      if(temp->right)
+        q.push(temp->right);
+    }
+  }
+  return count;
+}
+
+int main() {
+  node* root;
+  root = create();
+  cout<<"\nTotal leaves : "<<totalLeaves(root);
+  return 0;
+}

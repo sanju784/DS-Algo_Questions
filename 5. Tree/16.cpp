7549ce7f4f8d35d4c627100c6afb4b442986cb6d
sanju784 <sanjunitr51@gmail.com>
2017-01-18 20:13:41 +0900

    Node with one child binary tree

---
+++ 5. Tree/16.cpp
@@ -0,0 +1,67 @@
+/*********************************************
+*
+*  Node with one child binary tree
+*
+*/////////////////////////////////////////////
+
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct tree {
+  int data;
+  struct tree *left, *right;
+} node;
+
+queue <node*> q;
+
+node *create() {
+  node* p;
+  int n;
+  cout<<"\nEnter data (Enter -1 if no child node) : ";
+  cin>>n;
+  if(n == -1) {
+    return NULL;
+  }
+  p = (node*)malloc(sizeof(node));
+  p->data = n;
+  cout<<"\nEnter left child of "<<n;
+  p->left = create();
+  cout<<"\nEnter right child of "<<n;
+  p->right = create();
+  return p;
+}
+
+int countHalfNode(node *p) {
+  if(!p)return 0;
+  q.push(p);
+  node *temp;
+  int count = 0;
+  while(!q.empty()) {
+    temp = q.front();
+    q.pop();
+    if (temp->left && temp->right) {
+      q.push(temp->left);
+      q.push(temp->right);
+    } else {
+      if(temp->left) {
+        q.push(temp->left);
+        count++;
+      }
+      if(temp->right) {
+        q.push(temp->right);
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+int main() {
+  node* root;
+  root = create();
+  cout<<"\nTotal half nodes : "<<countHalfNode(root);
+  return 0;
+}

43ec1e5e4edb8b98cbea384975897a079741e831
sanju784 <sanjunitr51@gmail.com>
2017-05-10 17:53:01 +0900

    Find Maximum span in an Array

---
+++ 3. Stack/11.c
@@ -0,0 +1,22 @@
+/*********************************************
+*
+*  Find Maximum span in an Array
+*
+*/////////////////////////////////////////////
+
+#include <stdio.h>
+
+void main() {
+	int a[5] = {6,3,4,5,2};
+	int span[5], i;
+	for(i = 0;i < 5; i++) {
+		int j = 1;
+		while(j <= i && a[i] > a[i-j]) {
+			j++;
+		}
+		span[i] = j;
+	}
+
+	for (i = 0; i < 5;i++)
+		printf("%d  ", span[i]);
+}

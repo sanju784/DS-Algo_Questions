a8a2729281beebf5743caad217663b84a1823b4f
sanju784 <sanjunitr51@gmail.com>
2017-08-02 01:41:38 +0900

    Check who wins election by counting votes

---
+++ 8. Sort/3.c
@@ -0,0 +1,28 @@
+/*********************************************
+*
+*  Check who wins election by counting votes
+*
+*/////////////////////////////////////////////
+
+#include <stdio.h>
+
+void main() {
+  int i;
+  int candidates[] = {0,1,2};
+  int votes[] = {0,2,1,0,0,2,2,2};
+  int n = sizeof(votes)/sizeof(votes[0]);
+  int c[] = {0,0,0};
+  for(i=0;i<n;i++) {
+    if(votes[i] == 0)
+        c[0]++;
+    else if(votes[i] == 1)
+        c[1]++;
+    else
+        c[2]++;
+  }
+  int win = 0;
+  for (i=1;i<3;i++)
+    if(c[win] < c[i])
+        win = i;
+  printf("\nThe winner is %d", win);
+}

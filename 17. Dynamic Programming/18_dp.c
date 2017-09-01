a4710fb51a0ff030e21fc53583c989d5812e96d8
sanjeev <sanjunitr51@gmail.com>
2017-09-01 13:53:15 +0900

    0/1 knapsack problem

---
+++ 17. Dynamic Programming/18_dp.c
@@ -0,0 +1,41 @@
+/*********************************************
+*
+*  0/1 Knapsack problem
+*
+*/////////////////////////////////////////////
+
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    return (a>b)?a:b;
+}
+
+int getMaximumKnapsack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int k[n+1][W+1];
+
+    for(i=0;i<=n;i++)
+    {
+        for(w=0;w<=W;w++)
+        {
+            if(i==0 || w==0)
+                k[i][w] = 0;
+            else if(wt[i-1] <= w)
+                k[i][w] = max(val[i-1] + k[i-1][w-wt[i-1]], k[i-1][w]);
+            else
+                k[i][w] = k[i-1][w];
+        }
+    }
+    return k[n][W];
+}
+
+void main()
+{
+    int val[] = {60,100,120};
+    int wt[] = {10,20,30};
+    int W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    printf("Maximum value = %d", getMaximumKnapsack(W, wt, val, n));
+}
---
+++ 17. Dynamic Programming/18_rec.c
@@ -0,0 +1,34 @@
+/*********************************************
+*
+*  0/1 knapsack problem - By Recursion
+*
+*/////////////////////////////////////////////
+
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    return (a>b)?a:b;
+}
+
+int getMaximumKnapsack(int W, int wt[], int val[], int n)
+{
+    if(n==0 || W ==0)
+        return 0;
+
+    if(wt[n-1] > W)
+        return getMaximumKnapsack(W, wt, val, n-1);
+
+    else
+        return max(val[n-1] + getMaximumKnapsack(W-wt[n-1], wt, val, n-1),
+                              getMaximumKnapsack(W, wt, val, n-1));
+}
+
+void main()
+{
+    int val[] = {60,100,120};
+    int wt[] = {10,20,30};
+    int W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    printf("Maximum value = %d", getMaximumKnapsack(W, wt, val, n));
+}

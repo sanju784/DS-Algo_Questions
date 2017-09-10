4615950a82d7123a7ae54f4264ca58a5818616c0
sanjeev chaurasia <sanju784@users.noreply.github.com>
2017-09-10 19:30:25 +0900

    Find Shortest Path between 2 nodes in a BST

--- 5. Tree/48.cpp
+++ 5. Tree/48.cpp
@@ -30,7 +30,6 @@ node *newNode(int n) {
   2     6
  / \   / \
 1   3 5   7
-
 */
 node *createBST() {
   node *p = newNode(4);
@@ -43,7 +42,7 @@ node *createBST() {
   return p;
 }
 
-node* findLCA(node* root, int a, int b) {
+node* getLCA(node* root, int a, int b) {
   while(root != NULL) {
     if(a <= root->data && b >= root->data) {
       return root;
@@ -55,32 +54,27 @@ node* findLCA(node* root, int a, int b) {
   }
 }
 
-void printShortestPath(node *top, int n1, int n2) {
-  node *temp1 = top;
-  node *temp2 = top;
-  stack<int> s;
-  while(temp1->data != n1) {
-      if(n1 < temp1->data) {
-        temp1 = temp1->left;
-      } else {
-        temp1 = temp1->right;
-      }
-      s.push(temp1->data);
+void printShortestPath(node *lca, int n1, int n2) {
+  node *temp = lca;
+  if(n2 < n1)
+	  swap(n1, n2);
+  deque<int> dq;
+  dq.push_front(temp->data);
+    while(temp->data != n1) {
+      temp = temp->left;
+      dq.push_front(temp->data);
     }
-
-    while(!s.empty()) {
-      cout<<" "<<s.top();
-      s.pop();
+	temp = lca;
+    while(temp->data != n2) {
+        temp = temp->right;
+		dq.push_back(temp->data);
     }
-    cout<<" "<<top->data;
-    while(temp2->data != n2) {
-      if(n2 < temp2->data) {
-        temp2 = temp2->left;
-      } else {
-        temp2 = temp2->right;
-      }
-      cout<<" "<<temp2->data;
-    }
+	
+	while(!dq.empty())
+	{
+		cout<<dq.front()<<" ";
+		dq.pop_front();
+	}
 }
 
 int main() {
@@ -93,7 +87,8 @@ int main() {
     n1 = n2;
     n2 = n;
   }
-  node *res = findLCA(root, n1, n2);
+  //getting LCA as path passes through it
+  node *res = getLCA(root, n1, n2);
   if (res) {
     printShortestPath(res, n1, n2);
   } else {

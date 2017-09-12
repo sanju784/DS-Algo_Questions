0d71e0631f1d83e5994051dd6c4556ca9fe6793a
sanjeev chaurasia <sanju784@users.noreply.github.com>
2017-09-12 19:47:42 +0900

    Find first element that is repeated

--- 9. Search/9.java
+++ 9. Search/9.java
@@ -16,6 +16,8 @@ class FirstRepeat{
 	public static void printFirstRepeat(int arr[]) {
 		HashSet<Integer> hash = new HashSet<>();
 		int min =-1;
+		//checking array from behind and saving the index of last repeating number from back
+		//so it will give first repeating number from front
 		for(int i = arr.length-1;i>=0;i--){
 			if(hash.contains(arr[i]))
 				min = i;
@@ -28,3 +30,23 @@ class FirstRepeat{
 			System.out.println("First Repeating element is " + arr[min]);
 	}
 }
+
+//in c++ using unordered_set
+//compile using g++ -std=c++0x file.cpp
+/**
+void firstR(int arr[], int n)
+{
+	unordered_set<int> s;
+	
+	int min = -1;
+	for(int i=n-1;i>=0;i--)
+	{
+		if(s.find(arr[i]) == s.end())
+			s.insert(arr[i]);
+		else
+			min =i;
+	}
+	
+	cout<<min;
+}
+*/

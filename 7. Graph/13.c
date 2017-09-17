7463d99c27b81324cca9dbc097a8d2063578ccfc
sanjeev chaurasia <sanju784@users.noreply.github.com>
2017-09-17 15:11:15 +0900

    All pair shortest Path

--- 7. Graph/13.c
+++ 7. Graph/13.c
@@ -62,11 +62,12 @@ void floydWarshall(int graph[][V])
 
 void main()
 {
-	int graph[V][V] = { {  0,   5, INF,   10},
-						{INF,   0,   3,  INF},
-						{INF, INF,   0,    1},
-						{INF, INF, INF,    0}
-					   };
+	int graph[V][V] = { 
+				{  0,   5, INF,   10},
+				{INF,   0,   3,  INF},
+				{INF, INF,   0,    1},
+				{INF, INF, INF,    0}
+			  };
 					   
 	floydWarshall(graph);
 }
